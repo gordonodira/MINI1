c040c936eab4427014f79d937178ab19baca08a6
gordonodira <go37@njit.edu>
2018-10-13 11:32:54 -0400

    1.Task:Initial Set Up 2. class main 3.class csv -

--- public/index.php
+++ public/index.php
@@ -15,7 +15,27 @@ class main {
 
 
 class html{}
-class csv{}
+class csv{
+    static public function getRecords ($filename){
+        $file=fopen($filename,"r");
+        $fieldNames =array();
+
+        $count=0;
+
+        while ( ! feof($file)){
+
+            $record=fgetcsv($file);
+            if($count==0){
+                $fieldNames=$record;
+            }else{
+                $records[]=recordFactory::create($fieldNames,$record);
+            }
+            $count++;
+        }
+        fclose($file);
+        return $records;
+    }
+}
 
 class record{}
 class recordFactory{}
