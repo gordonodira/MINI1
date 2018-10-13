1b3396453d202ec814c3fa1d2cd6ff5a5d8e3d13
gordonodira <go37@njit.edu>
2018-10-13 11:19:32 -0400

    1.Task:Initial Set Up 2. class main

--- public/index.php
+++ public/index.php
@@ -3,3 +3,19 @@
  * Created by PhpStorm.
 
  */
+
+main::start("project.csv");
+class main {
+    public static function start ($filename){
+        $records=csv::getRecords($filename);
+        $table =html::generateTable($records);
+        echo $table;
+    }
+}
+
+
+class html{}
+class csv{}
+
+class record{}
+class recordFactory{}
